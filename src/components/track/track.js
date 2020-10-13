ce77f05ca3bf1dd61a7740d92a89966c06a0b767
Matthias Heisterkamp <hello@mhstrkmp.dev>
2020-10-13 16:54:41 +0200

    Refactoring

--- src/components/track/track.js
+++ src/components/track/track.js
@@ -1,28 +1,15 @@
 import "./track.css";
 import imgSrc from "../../assets/play-track.svg";
 
-/* Outline of HTML Structure
-
-<div class="track">
-  <img class="track__image" src="../image.png" />
-  <h3 class="track__title">Billie Jean</h3>
-  <p class="track__artist">Michael Jackson</p>
-  <button>
-    <img src="../play-track.svg" />
-  </button>
-</div>
-
-*/
-
 export function createTrackElement(track) {
-  const trackComponent = document.createElement("div"); // creates a <div></div>
+  const trackComponent = document.createElement("div");
   trackComponent.className = "track";
 
-  const titleElement = document.createElement("h3"); // creates a <h3></h3>
-  titleElement.innerText = track.title; // <h3>Billie Jean</h3>
+  const titleElement = document.createElement("h3");
+  titleElement.innerText = track.title;
 
-  const artistElement = document.createElement("p"); // creates a <p></p>
-  artistElement.innerText = track.artist; // <p>Michael Jackson</p>
+  const artistElement = document.createElement("p");
+  artistElement.innerText = track.artist;
 
   const descriptionElement = document.createElement("div");
   descriptionElement.className = "track__description";
@@ -38,8 +25,10 @@ export function createTrackElement(track) {
   const primaryBtnImage = document.createElement("img");
   primaryBtnImage.src = imgSrc;
   primaryBtn.append(primaryBtnImage);
+
   trackComponent.append(titleImage, descriptionElement, primaryBtn);
   const audioElement = new Audio(track.audioSrc);
+
   primaryBtn.onclick = function () {
     audioElement.play();
   };
