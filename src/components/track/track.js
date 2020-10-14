24a856eca9033def0df80fb79934f9c895ce9ca3
Matthias Heisterkamp <hello@mhstrkmp.dev>
2020-10-14 11:15:28 +0200

    Add pause functionality

--- src/components/track/track.js
+++ src/components/track/track.js
@@ -1,5 +1,6 @@
 import "./track.css";
-import imgSrc from "../../assets/play-track.svg";
+import imgPlaySrc from "../../assets/play-track.svg";
+import imgPauseSrc from "../../assets/pause-track.svg";
 
 export function createTrackElement(track) {
   const trackComponent = document.createElement("div");
@@ -23,14 +24,28 @@ export function createTrackElement(track) {
   const primaryBtn = document.createElement("button");
   primaryBtn.className = "track__button";
   const primaryBtnImage = document.createElement("img");
-  primaryBtnImage.src = imgSrc;
+  primaryBtnImage.src = imgPlaySrc;
   primaryBtn.append(primaryBtnImage);
 
   trackComponent.append(titleImage, descriptionElement, primaryBtn);
   const audioElement = new Audio(track.audioSrc);
 
+  /* Player function to play and pause onclick */
+
+  let isPlaying = false;
   primaryBtn.onclick = function () {
-    audioElement.play();
+    if (isPlaying) {
+      audioElement.pause();
+      primaryBtnImage.src = imgPlaySrc;
+      primaryBtnImage.alt = "Play Button";
+      console.log("It's pausing");
+    } else {
+      audioElement.play();
+      primaryBtnImage.src = imgPauseSrc;
+      primaryBtnImage.alt = "Pause Button";
+      console.log("It's playing");
+    }
+    isPlaying = !isPlaying;
   };
   return trackComponent;
 }
