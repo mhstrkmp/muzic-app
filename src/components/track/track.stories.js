cb9d60d132c85e32a9b87de0334598f7e146d42e
Matthias Heisterkamp <hello@mhstrkmp.dev>
2020-10-14 16:26:18 +0200

    Refactoring

--- src/components/track/track.stories.js
+++ src/components/track/track.stories.js
@@ -5,28 +5,6 @@ export default {
   parameters: { layout: "centered" },
 };
 
-/*
-export const songOne = () =>
-  createTrackElement({
-    title: "Websiiite feat Dragan",
-    artist: "Tracky Birthday",
-    imgSrc:
-      "https://files.freemusicarchive.org/storage-freemusicarchive-org/images/artists/Tracky_Birthday_-_2009113015813089.jpg?width=290&height=290",
-    audioSrc:
-      "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/Ego_Twister/Tracky_Birthday/Greatest_It_va_2010/Tracky_Birthday_-_05_-_Websiiite_feat_Dragan.mp3",
-  });
-
-export const songTwo = () =>
-  createTrackElement({
-    title: "8bitDetective",
-    artist: "Bass Assassin",
-    imgSrc:
-      "https://files.freemusicarchive.org/storage-freemusicarchive-org/images/albums/IllocanbloBass_Assassin_-_Peony_Lantern_Split_EP_-_2011060485057279.jpg?method=crop&width=290&height=290",
-    audioSrc:
-      "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/Progolog/Bass_Assassin/Peony_Lantern_Split_EP/Bass_Assassin_-_02_-_8bitDetective.mp3",
-  });
-*/
-
 let songs = [
   {
     title: "Websiiite feat Dragan",
